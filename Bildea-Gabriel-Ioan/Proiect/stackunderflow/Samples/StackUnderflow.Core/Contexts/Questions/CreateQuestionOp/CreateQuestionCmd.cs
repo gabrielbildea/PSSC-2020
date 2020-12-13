d18920f4195214bf5d31f4529a57ff359c124c9b
gabrielbildea <gabibildea@gmail.com>
2020-12-14 00:18:54 +0200

    Added CreateQuestion workflow

---
+++ Bildea-Gabriel-Ioan/Proiect/stackunderflow/Samples/StackUnderflow.Core/Contexts/Questions/CreateQuestionOp/CreateQuestionCmd.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackUnderflow.Domain.Schema.Questions.CreateQuestionOp
+{
+    public class CreateQuestionCmd
+    {
+        public CreateQuestionCmd() { }
+        public CreateQuestionCmd(string Title, string Body, string[] Tags)
+        {
+            this.Title = Title;
+            this.Body = Body;
+            this.Tags = Tags;
+        }
+
+        [Required(ErrorMessage = "Title is missing.")]
+        [MinLength(15), MaxLength(150)]
+        public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Body is missing.")]
+        [MinLength(30), MaxLength(30000)]  //Body is limited to 30000 characters;
+        public string Body { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter at least one tag; see a list of popular tags.")]
+        [MinLength(1), MaxLength(10)]
+        public string[] Tags { get; set; }
+
+
+    }
+}
---
+++ Bildea-Gabriel-Ioan/Proiect/stackunderflow/Samples/StackUnderflow.Core/Contexts/Questions/CreateQuestionOp/CreateQuestionResult.cs
@@ -0,0 +1,41 @@
+﻿using CSharp.Choices;
+using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.Domain.Schema.Questions.CreateQuestionOp
+{
+    [AsChoice]
+    public static partial class CreateQuestionResult
+    {
+        public interface ICreateQuestionResult { }
+
+        public class QuestionCreated : ICreateQuestionResult
+        {
+ 
+            public Guid QuestionId { get; set; }
+            public string Title { get; set; }
+            public string Body { get; set; }
+            public string[] Tags { get; set; }
+
+            public QuestionCreated(Guid QuestionId, string Title, string Body, string[] Tags)
+            {
+                this.QuestionId = QuestionId;
+                this.Title = Title;
+                this.Body = Body;
+                this.Tags = Tags;
+            }
+        }
+        public class QuestionNotCreated : ICreateQuestionResult
+        {
+            public string Reason { get; set; }
+
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+    }
+   
+}
